e10e9ca4ade2465587d7c53985663b200151b09f
tung-lin <im_fish@msn.com>
2018-02-06 09:28:00 +0800

    check email address format using regex

--- plugin/action/email/emailplugin.go
+++ plugin/action/email/emailplugin.go
@@ -1,9 +1,12 @@
 package email
 
 import (
-	"EventFlow/common/tool/arraytool"
 	"EventFlow/common/tool/parametertool"
 	"fmt"
+	"log"
+	"net/smtp"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -11,16 +14,19 @@ type EmailPlugin struct {
 	Setting SettingConfig
 }
 
+var emailRegexp = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
 func (trigger *EmailPlugin) FireAction(messageFromTrigger *string, parameters *map[string]interface{}) {
 
 	dynamicto := parametertool.ReplaceWithParameter(&trigger.Setting.DynamicTo, parameters)
+
 	to := append(trigger.Setting.StaticTo, strings.Split(dynamicto, ",")...)
-	to = arraytool.RemoveItem(to, "")
+	to = checkEmailAddress(&to)
 
 	subject := parametertool.ReplaceWithParameter(&trigger.Setting.Subject, parameters)
 	body := parametertool.ReplaceWithParameter(&trigger.Setting.Body, parameters)
-	//smtpServer := trigger.Setting.Address + ":" + strconv.Itoa(trigger.Setting.Port)
-	//auth := smtp.PlainAuth("", trigger.Setting.Username, trigger.Setting.Password, trigger.Setting.Address)
+	smtpServer := trigger.Setting.Address + ":" + strconv.Itoa(trigger.Setting.Port)
+	auth := smtp.PlainAuth("", trigger.Setting.Username, trigger.Setting.Password, trigger.Setting.Address)
 
 	header := ""
 	header += fmt.Sprintf("From: %s\r\n", trigger.Setting.From)
@@ -28,9 +34,20 @@ func (trigger *EmailPlugin) FireAction(messageFromTrigger *string, parameters *m
 	header += fmt.Sprintf("Subject: %s\r\n", subject)
 	header += "\r\n" + body
 
-	//err := smtp.SendMail(smtpServer, auth, trigger.Setting.From, to, []byte(header))
+	err := smtp.SendMail(smtpServer, auth, trigger.Setting.From, to, []byte(header))
 
-	//if err != nil {
-	//	log.Printf("[action][email] send email failed: %v", err)
-	//}
+	if err != nil {
+		log.Printf("[action][email] send email failed: %v", err)
+	}
+}
+
+func checkEmailAddress(emailAddresses *[]string) (results []string) {
+
+	for _, email := range *emailAddresses {
+		if email != "" && emailRegexp.MatchString(email) {
+			results = append(results, email)
+		}
+	}
+
+	return
 }
